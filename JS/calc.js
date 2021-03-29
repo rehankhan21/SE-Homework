89c55382ddc5fafe7e162f4ece11708f6a8c6b91
rehankhan21 <narutop224@gmail.com>
2021-03-29 11:59:39 -0400

    evenodd

--- JS/calc.js
+++ JS/calc.js
@@ -1,7 +1,17 @@
-var vote = 18;
+/* var vote = 18;
 
 if (vote >= 18){
-    console.log("you can vote");
+     console.log("you can vote");
 }else {
-    console.log("you cannot vote");
+     console.log("you cannot vote");
+} */
+
+
+var x = 0;
+
+if((x % 2) == 0){
+    console.log("is even");
+}
+else {
+    console.log("is odd")
 }
