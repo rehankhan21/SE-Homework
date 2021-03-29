4dd70ccf1d46a4db5a6374d7f0f41545d66264bf
rehankhan21 <narutop224@gmail.com>
2021-03-29 12:05:26 -0400

    nothing

--- JS/calc.js
+++ JS/calc.js
@@ -7,11 +7,3 @@ if (vote >= 18){
 } */
 
 
-var x = 0;
-
-if((x % 2) == 0){
-    console.log("is even");
-}
-else {
-    console.log("is odd")
-}
---
+++ JS/evenodd.js
@@ -0,0 +1,8 @@
+var x = 0;
+
+if((x % 2) == 0){
+    console.log("is even");
+}
+else {
+    console.log("is odd");
+}
