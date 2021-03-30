32b8ee3b7055368844e31525193b50192149c9e8
rehankhan21 <narutop224@gmail.com>
2021-03-30 11:17:18 -0400

    uppercase

--- JS/vowel.js
+++ JS/vowel.js
@@ -1,7 +1,12 @@
-var letter = 'u';
+var letter = 'I';
+var lower = letter.toLowerCase();
+var upper = letter.toUpperCase();
 
 if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
-    console.log(letter + " is a vowel");
+    console.log(letter + " is a lowercase vowel");
+}
+else if (letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U'){
+    console.log(letter + " is a uppercase vowel");
 }
 else {
     console.log(letter + " is a consonant");
