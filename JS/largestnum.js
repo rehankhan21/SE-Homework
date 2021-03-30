9238be8bc57f09ab0895db50d73ab48d912d26a6
rehankhan21 <narutop224@gmail.com>
2021-03-30 09:13:38 -0400

    largestnum

---
+++ JS/largestnum.js
@@ -0,0 +1,13 @@
+var num1 = 4;
+var num2 = 8;
+var num3 = 9;
+
+if (num1 > num2 && num1 > num3){
+    console.log(num1);
+}
+else if (num2 > num1 && num2 > num3) {
+    console.log(num2);
+}
+else {
+    console.log(num3);
+}
