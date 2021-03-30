8851ee2c0680b1e3182ccb2c74092e40f1021413
rehankhan21 <narutop224@gmail.com>
2021-03-30 12:16:50 -0400

    nestedif

---
+++ JS/nestedif.js
@@ -0,0 +1,17 @@
+var num = "21";
+
+if(num > 0){
+    if(num <= 100) {
+        console.log(num + " less than 100");
+    }
+    else{
+        console.log(num + " is greater than 100");
+    }
+}
+else if (num < 0){
+
+        console.log(num + " is negative");
+}
+else {
+    console.log(num + " is zero");
+}
--- JS/vowel.js
+++ JS/vowel.js
@@ -1,15 +1,16 @@
-var letter = '5';
-var lower = letter.toLowerCase();
+//var name = prompt("enter your name : ");
+var letter = '8';
+//var lower = letter.toLowerCase();
 var upper = letter.toUpperCase();
-//var num =  parseInt(letter);
+var num =  parseInt(letter);
 
 if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
     console.log(letter + " is a lowercase vowel");
 }
-else if (letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U'){
+else if (upper == 'A' || upper == 'E' || upper == 'I' || upper == 'O' || upper == 'U'){
     console.log(letter + " is a uppercase vowel");
 }
-else if (letter > 0 && letter < 9) {
+else if (num >= 0 && num <= 9) {
     console.log(letter + " is a number");
 }
 else {
