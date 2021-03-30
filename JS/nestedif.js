4e55e1c5f7e3e215673d75728ab4311b0852e3fa
rehankhan21 <narutop224@gmail.com>
2021-03-30 14:05:46 -0400

    fixednestedif

--- JS/nestedif.js
+++ JS/nestedif.js
@@ -1,20 +1,21 @@
-var grade = 80;
+var grade = -1;
 
 if(grade > 60){
-    if(grade >= 90 && grade <= 100) {
+
+    if(grade >= 90) {
         console.log(" you are a top student");
     }
     else{
-        console.log(" almost to the top keeping going!!!");
+        console.log(" almost to the top keep going!!!");
     }
 }
-else if (grade >= 0 && grade < 60){
+else if (grade >= 0 && grade <= 60){
 
-       if(grade > 50 && grade <= 60) {
+       if(grade > 50) {
           console.log(" you have to try alittle harder"); 
        } 
        else {
-           console.log("you have to try ALOT harder")
+           console.log(" you have to try ALOT harder")
        }
 }
 else {
