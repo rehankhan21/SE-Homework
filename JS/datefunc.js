49ea87378e78bd66a3d095292b0352184d92d77f
rehankhan21 <narutop224@gmail.com>
2021-03-30 10:35:44 -0400

    vowel

--- JS/datefunc.js
+++ JS/datefunc.js
@@ -1,13 +1,17 @@
 var date = new Date();
-//date.getMinutes()
+
 var hour = date.getHours();
+var min = date.getMinutes()
+var sec = date.getSeconds();
 
 if(hour < 12) {
     console.log("Good Morning");
+    console.log(hour + ":" + min + ":" + sec);
 }
 else if (hour >= 12 && hour <= 16) {
     console.log("Good Afternoon");
 }
 else {
     console.log("Hello, how are you?");
+   
 }
---
+++ JS/vowel.js
@@ -0,0 +1,8 @@
+var letter = 'u';
+
+if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+    console.log(letter + " is a vowel");
+}
+else {
+    console.log(letter + " is a constant");
+}
