3e23473e3fb403dc0f1ef1d596b30f338d768242
rehankhan21 <narutop224@gmail.com>
2021-03-30 11:43:37 -0400

    letternum

--- JS/vowel.js
+++ JS/vowel.js
@@ -1,12 +1,16 @@
-var letter = 'I';
+var letter = '5';
 var lower = letter.toLowerCase();
 var upper = letter.toUpperCase();
+//var num =  parseInt(letter);
 
 if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
     console.log(letter + " is a lowercase vowel");
 }
 else if (letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U'){
     console.log(letter + " is a uppercase vowel");
+}
+else if (letter > 0 && letter < 9) {
+    console.log(letter + " is a number");
 }
 else {
     console.log(letter + " is a consonant");
