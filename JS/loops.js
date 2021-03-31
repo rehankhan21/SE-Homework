8c8055cbebcca345cd16a44a57aef8d948f8f508
rehankhan21 <narutop224@gmail.com>
2021-03-31 11:18:16 -0400

    loops

--- JS/loops.js
+++ JS/loops.js
@@ -1,8 +1,6 @@
-
-
 for(let counter = 1; counter <= 10; counter++) {
 
-    console.log("Hello!! How are you today");
+    console.log(counter + ". Hello!! How are you today");
 }
 
 console.log("Outside of for loop now")
