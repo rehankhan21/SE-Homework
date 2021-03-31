bb39f522950ad78a0f6349433f1543d069255b18
rehankhan21 <narutop224@gmail.com>
2021-03-31 09:48:30 -0400

    ifmonths

---
+++ JS/ifmonths.js
@@ -0,0 +1,71 @@
+var month = 1;
+var monthName = "";
+var day = 7;
+var dayName = "";
+
+if(month == 1){
+    monthName = "janurary"
+}
+else if(month == 2){
+    monthName = "Feburary"
+}
+else if(month == 3){
+    monthName = "March"
+}
+else if(month == 4){
+    monthName = "April"
+}
+else if(month == 5){
+    monthName = "May"
+}
+else if(month == 6){
+    monthName = "June"
+}
+else if(month == 7){
+    monthName = "July"
+}
+else if(month == 8){
+    monthName = "August"
+}
+else if(month == 9){
+    monthName = "September"
+}
+else if(month == 10){
+    monthName = "October"
+}
+else if(month == 11){
+    monthName = "November"
+}
+else if(month == 12){
+    monthName = "December"
+}
+else{
+    monthName = "Invalid Month"
+}
+
+if(day == 1){
+    dayName = "Sunday"
+}
+else if (day == 2){
+    dayName = "Monday"
+}
+else if (day == 3) {
+    dayName = "Tuesday"
+}
+else if (day == 4) {
+    dayName = "Wenesday"
+}
+else if (day == 5) {
+    dayName = "Thursday"
+}
+else if (day == 6) {
+    dayName = "Friday"
+}
+else if (day == 7) {
+    dayName = "Saturday"
+}
+else {
+    dayName = "Invalid Day"
+}
+
+console.log("It is " + monthName + " on " + dayName );
---
+++ JS/ifstatement.js
@@ -0,0 +1,135 @@
+var month = 4;
+var day = 5; // valid 1 thru 7, Sunday - Saturday
+
+if (month == 1){
+
+}
+else if (month == 2){
+    if(day == 1){
+        console.log("It is Feburary on Sunday")
+    }
+    else if (day == 2){
+        console.log("It is Feburary on Monday")
+    }
+    else if (day == 3){
+        console.log("It is Feburary on Tuesday")
+    }
+    else if (day == 4){
+        console.log("It is Feburary on Wenesday")
+    } 
+    else if (day == 5){
+        console.log("It is Feburary on Thursday")
+    }
+    else if (day == 6){
+        console.log("It is Feburary on Friday")
+    }
+    else if (day == 7){
+        console.log("It is Feburary on Sunday")
+    }
+    else {
+        console.log("not a valid day")
+    }
+}
+else if (month == 3){
+    if(day == 1){
+        console.log("It is March on Sunday")
+    }
+    else if (day == 2){
+        console.log("It is March on Monday")
+    }
+    else if (day == 3){
+        console.log("It is March on Tuesday")
+    }
+    else if (day == 4){
+        console.log("It is March on Wenesday")
+    } 
+    else if (day == 5){
+        console.log("It is March on Thursday")
+    }
+    else if (day == 6){
+        console.log("It is March on Friday")
+    }
+    else if (day == 7){
+        console.log("It is March on Sunday")
+    }
+    else {
+        console.log("not a valid day")
+    }
+}
+else if (month == 4){
+    if(day == 1){
+        console.log("It is April on Sunday")
+    }
+    else if (day == 2){
+        console.log("It is April on Monday")
+    }
+    else if (day == 3){
+        console.log("It is April on Tuesday")
+    }
+    else if (day == 4){
+        console.log("It is April on Wenesday")
+    } 
+    else if (day == 5){
+        console.log("It is April on Thursday")
+    }
+    else if (day == 6){
+        console.log("It is April on Friday")
+    }
+    else if (day == 7){
+        console.log("It is April on Sunday")
+    }
+    else {
+        console.log("not a valid day")
+    }
+}
+else if (month == 5){
+    if(day == 1){
+        console.log("It is May on Sunday")
+    }
+    else if (day == 2){
+        console.log("It is May on Monday")
+    }
+    else if (day == 3){
+        console.log("It is May on Tuesday")
+    }
+    else if (day == 4){
+        console.log("It is May on Wenesday")
+    } 
+    else if (day == 5){
+        console.log("It is May on Thursday")
+    }
+    else if (day == 6){
+        console.log("It is May on Friday")
+    }
+    else if (day == 7){
+        console.log("It is May on Sunday")
+    }
+    else {
+        console.log("not a valid day")
+    }
+}
+
+if(day == 1){
+    console.log("It is Sunday")
+}
+else if (day == 2){
+    console.log("It is January on Monday")
+}
+else if (day == 3){
+    console.log("It is January on Tuesday")
+}
+else if (day == 4){
+    console.log("It is January on Wenesday")
+} 
+else if (day == 5){
+    console.log("It is January on Thursday")
+}
+else if (day == 6){
+    console.log("It is January on Friday")
+}
+else if (day == 7){
+    console.log("It is January on Sunday")
+}
+else {
+    console.log("not a valid day")
+}
