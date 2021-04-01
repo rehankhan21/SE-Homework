dd95b4f1de42b512271e3bfc92df9fa04fca88cb
rehankhan21 <narutop224@gmail.com>
2021-04-01 09:29:23 -0400

    fizzbuzz

--- JS/loopfindnum.js
+++ JS/loopfindnum.js
@@ -1,11 +1,11 @@
 for(let counter = 1; counter <= 50; counter++){
     if(counter % 3 == 0 && counter % 5 == 0){
-        console.log(counter + " fizzbuzz")
+        console.log(" fizzbuzz")
     }
     else if(counter % 5 == 0){
-        console.log(counter + " buzz")
+        console.log("buzz")
     }
     else if (counter % 3 == 0) {
-        console.log(counter + " fizz");
+        console.log("fizz");
     }
 }
