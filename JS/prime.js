4405dccbce9b9ab3640250fa15ebcabcc21d98d9
rehankhan21 <narutop224@gmail.com>
2021-04-05 11:58:05 -0400

    primeloop

--- JS/prime.js
+++ JS/prime.js
@@ -1,15 +1,15 @@
 let num = 15;
 let flag = true;
 
-for(let counter = 2; counter < num; counter++){
-    
-         if(num % counter == 0){
-            flag = false;
-            break;
-        }
+for (let counter = 2; counter < num; counter++) {
+
+    if (num % counter == 0) {
+        flag = false;
+        break;
+    }
 }
 
-if(flag == true){
+if (flag == true) {
     console.log("this is a prime number")
 }
 else {
