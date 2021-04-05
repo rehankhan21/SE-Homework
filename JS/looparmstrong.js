06223fa7659b0a2dad00e88e92e0c0ad1d53c337
rehankhan21 <narutop224@gmail.com>
2021-04-05 15:23:34 -0400

    whileArms

--- JS/looparmstrong.js
+++ JS/looparmstrong.js
@@ -1,19 +1,35 @@
-let armnum = 123;
+let armnum = 153;
 let armtest = armnum;
 let singlenum = 0;
 let total = 0;
+let q = 0;
+let r = 0;
 
- for(let i = 1; i <= 3; i++) {
+// for (let i = 1; i <= 3; i++) {
 
-    singlenum = armtest % 10;
-    armtest = Math.trunc(armtest / 10);
+//     singlenum = armtest % 10;
+//     armtest = Math.trunc(armtest / 10);
 
-     total += Math.pow(singlenum, 3);  
+//     total += Math.pow(singlenum, 3);
+
+//     q = num / 10;
+//     r = 
+
+
+// }
+
+while(armtest != 0){
+
+     singlenum = armtest % 10;
+     armtest = Math.trunc(armtest / 10);
+
+     total += Math.pow(singlenum, 3);
+
 }
 
-if(total == armnum){
+if (total == armnum) {
     console.log(armnum + " is an armstrong number")
 }
-else{
+else {
     console.log(armnum + " is not an armstrong numbner")
 }
