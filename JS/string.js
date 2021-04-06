b6e25a99df85c9752d182b0a94d406fa8b6960e4
rehankhan21 <narutop224@gmail.com>
2021-04-06 11:18:23 -0400

    string

--- JS/string.js
+++ JS/string.js
@@ -3,12 +3,18 @@ let string = "John Smith";
 console.log(string);
 
 let fname = "John ";
-let lname = "Smith S";
+let lname = "Smith";
+let fullname = fname.concat(lname);
 
-console.log(fname.concat(lname));
+/*console.log(fname.concat(lname));
 
 console.log(fname.toUpperCase(), lname.toLowerCase());
 
 console.log(fname.indexOf(' '));
 
-console.log("t is at index " + lname.indexOf("t"), "S last index is " + lname.lastIndexOf("S"));
+console.log("t is at index " + lname.indexOf("t"), "S last index is " + lname.lastIndexOf("S"));*/
+
+for(let i = 0; i < fullname.length; i++) {
+
+    console.log(fullname[i]);
+}
