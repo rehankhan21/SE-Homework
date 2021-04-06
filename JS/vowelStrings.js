762b48535cc50cf715194f68612f04ebcfd04b55
rehankhan21 <narutop224@gmail.com>
2021-04-06 14:32:19 -0400

    vowelStrings

--- JS/vowelStrings.js
+++ JS/vowelStrings.js
@@ -1,10 +1,11 @@
 let string = "Hellov";
+let stringlower = string.toLowerCase();
 let vowels = "";
 let numOfVowels = 0;
 
 for (let i = 0; i < string.length; i++) {
 
-    let v = string[i];
+    let v = stringlower[i];
 
     if(v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u'  ) {
         console.log(vowels.concat(v));
