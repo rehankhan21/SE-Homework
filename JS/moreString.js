a7d22e9834f2103a6d5a4861df5d6a0b07122c4a
rehankhan21 <narutop224@gmail.com>
2021-04-07 12:02:08 -0400

    stringmethods

---
+++ JS/moreString.js
@@ -0,0 +1,33 @@
+let str = " I like hot coffee hot  "
+let strPadStart = "padding in the start"
+let strPadEnd = "padding in the end"
+let newline = "first line \nnew line"
+
+console.log(str.trim());
+
+let str2 = str.replace("hot", "cold")
+//let str3 = str.replaceAll("hot","cold")
+let str4 = str.replace(/hot/g, "cold")
+
+/*
+    padStart() adds padding b4 string
+    padEnd()
+*/
+console.log(str2)
+
+console.log(str4)
+
+console.log(strPadStart.padStart(40, " "))
+console.log(strPadEnd.padEnd(40, 1))
+
+console.log(newline);
+console.log('\n');
+
+let strConcat = strPadStart
+    .padEnd(strPadStart.length + 1, " ")
+    .concat(strPadEnd)
+    .replace(/padding/g, "coffee")
+    .concat(".")
+    .toUpperCase()
+
+console.log(strConcat);
---
+++ JS/tripleequal.js
@@ -0,0 +1,28 @@
+let strNum = "5";
+let num = 5;
+
+let str1 = "Tom";
+let str2 = "tom";
+
+if(str1 == str2) {
+    console.log(true)
+}
+else {
+     console.log(false)
+}
+
+console.log("----------")
+
+if ( strNum === num) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+if(strNum == num) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
