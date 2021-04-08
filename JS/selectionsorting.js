73d388851b11a41eabb4586ac8b36d3782365d7d
rehankhan21 <narutop224@gmail.com>
2021-04-08 14:58:06 -0400

    linearsorting

--- JS/selectionsorting.js
+++ JS/selectionsorting.js
@@ -1,7 +1,7 @@
 let array = [-1, 220, 10, 35, 5]
 
 
-for (let i = 0; i < array.length - 2; i++) {
+for (let i = 0; i < array.length - 1; i++) {
 
     for (let j = i + 1; j < array.length; j++) {
 
@@ -10,8 +10,10 @@ for (let i = 0; i < array.length - 2; i++) {
             tmp = array[i];;
             array[i] = array[j];
             array[j] = tmp;
+            
         }
     }
+    console.log(array[i])
 }
 
 console.log(array)
