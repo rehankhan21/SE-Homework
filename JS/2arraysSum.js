aae8194fd44455028fbe4060decd7759e85238e9
rehankhan21 <narutop224@gmail.com>
2021-04-08 13:30:35 -0400

    3arrays

--- JS/2arraysSum.js
+++ JS/2arraysSum.js
@@ -1,10 +1,12 @@
-let array1 = [3, 6, 3];
-let array2 = [2, 4, 3];
+let array1 = [];
+let array2 = [];
 let array3 = [];
 let sum = 0;
 
-for (let i = 0; i < array1.length; i++) {
+for (let i = 0; i < 5; i++) {
 
+    array1.push(i + 1);
+    array2.push(i * 2);
     sum = array1[i] + array2[i]
     array3.push(sum);
     console.log(array3[i]);
