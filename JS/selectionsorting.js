1ee85244915e23601915cd7ef6008e8d328d5836
rehankhan21 <narutop224@gmail.com>
2021-04-08 15:54:28 -0400

    buuble

--- JS/selectionsorting.js
+++ JS/selectionsorting.js
@@ -3,17 +3,17 @@ let array = [-1, 220, 10, 35, 5]
 
 for (let i = 0; i < array.length - 1; i++) {
 
-    for (let j = i + 1; j < array.length; j++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+
 
         let tmp = 0;
-        if(array[i] > array[j]) {
-            tmp = array[i];;
-            array[i] = array[j];
-            array[j] = tmp;
+        if(array[j] > array[j + 1]) {
+            tmp = array[j];;
+            array[j] = array[j + 1];
+            array[j + 1] = tmp;
             
         }
     }
-    console.log(array[i])
 }
 
 console.log(array)
