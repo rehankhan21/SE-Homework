37832b96bf3647823f99f41e9c36fa9dadea7fa7
rehankhan21 <narutop224@gmail.com>
2021-04-12 11:08:05 -0400

    string conversion

---
+++ JS/strArray.js
@@ -0,0 +1,32 @@
+/* Array methods
+
+    converting strings to an array
+    var days = "mondya,tues,wed"
+
+    splits the string up but is still only 1 element in the array.
+    //daysarray = days.split(",")
+    //daysarray = days.split()
+
+    converting an array into a string
+    //array.join("--") uses a delimiter, seperates the string elements with the delimiter
+    //array.toString()
+*/
+
+let array = ["mon", "tues", "wed"];
+
+let strArray = array.join("--")
+
+let string = "bob john hop"
+
+let arrayStr = string.split(" ")
+let arrayStr2 = string.split()
+
+// this parses each strings character as a seperate element
+let arrayStr3 = string.split("") 
+
+//  document.getElementById("para1").innerHTML = strArray;
+
+// document.getElementById("para2").innerHTML = array;
+
+console.log(strArray);
+console.log(arrayStr)
