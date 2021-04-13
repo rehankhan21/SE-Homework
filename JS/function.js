5a500b425b1a37ed6f747683a2578562ba830ad6
rehankhan21 <narutop224@gmail.com>
2021-04-13 09:40:00 -0400

    function

--- JS/function.js
+++ JS/function.js
@@ -9,6 +9,36 @@ function findSum() {
 }
 
 function findProduct(a, b) {
-    
+
     return a * b;
+}
+
+function findEven(a) {
+
+    //let bool = false;
+
+    if(a % 2 == 0){
+        return "Even";
+    }
+    else {
+        return "Odd";
+    }
+}
+
+function totalParam() {
+    
+    console.log( arguments.length);
+}
+
+totalParam(1,2,3)
+
+function reverseString(a) {
+    let newStr = "";
+    for(let i = (a.length-1); i >= 0; i--) {
+
+        newStr += a[i];
+        console.log(newStr)
+    }
+
+    return newStr;
 }
---
+++ JS/functionsHw.js
@@ -0,0 +1,7 @@
+maxOfTwoNumbers = (a, b) => {
+
+    if(a > b)
+    return a
+    else
+    return b
+}
