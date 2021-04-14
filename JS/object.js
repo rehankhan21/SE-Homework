7ab486bfd238281e34cb46385b5d83abe264bb2c
rehankhan21 <narutop224@gmail.com>
2021-04-14 12:57:10 -0400

    moreObject practice

--- JS/object.js
+++ JS/object.js
@@ -6,5 +6,37 @@ let person = {
 
     fullName: function() {
         return this.fname + " " + this.lname;
+    },
+
+    noises: ["did you see my credit card?", "doge to the moon", "hello"]
+}
+
+let student = {
+
+    fname: "Dennis",
+    lname: "Cargas",
+    subjects: ["math", "social studies", "english"],
+    grades: [80, 70, 90],
+    average: function() {
+        let avg = 0;
+        let total = 0;
+        for(let i = 0; i < this.grades.length; i++) {
+            
+            total += this.grades[i];
+        }
+
+        avg = total/this.grades.length;
+
+        return Math.floor(avg);
+    },
+
+    result: function() {
+        
+        if(this.average() > 60){
+            return "Student has passed"
+        }
+        else {
+            return "Student has failed"
+        }
     }
 }
