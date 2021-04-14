341311425c7ff064370737f7c396154a4b6c3f9e
rehankhan21 <narutop224@gmail.com>
2021-04-14 12:28:16 -0400

    objectsIntro

---
+++ JS/object.js
@@ -0,0 +1,10 @@
+// let person = {
+//     fname: "John",
+//     lname: "Smith",
+//     address: "101 somewhere street",
+//     age: 21,
+
+//     fullName: function() {
+//         return this.fname + " " + this.lname;
+//     }
+// }
