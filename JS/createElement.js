114442fd5dd834c2d275ac4e2726d11ed837eed2
rehankhan21 <narutop224@gmail.com>
2021-04-15 11:56:15 -0400

    creating elements

--- JS/createElement.js
+++ JS/createElement.js
@@ -12,6 +12,14 @@ function changeDom(div) {
     //div.append(changeNode)
 }
 
-function changeBack() {
+function changeString(div) {
+
+    //let change = document.createElement("p");
+    let changeNode = document.createTextNode(" :this is from the js file")
+    
+    
+    //div.parentNode.replaceChild(changeButton, div)
+    
+    div.append(changeNode)
 
 }
