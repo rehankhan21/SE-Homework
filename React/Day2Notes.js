c08f3c50a67350061f83b5560324b4fb3fbae2c4
rehankhan21 <narutop224@gmail.com>
2021-05-04 09:58:00 -0400

    React Props

--- React/Day2Notes.js
+++ React/Day2Notes.js
@@ -1,4 +1,13 @@
 /*
     Day 2 React 
-    
+
+    React functions return and pass properties as arguments, or PROPS.
+
+    Props is a KEYWORD, props is an object which has values and
+    properties.
+
+    Pass properties when you use the compoenent in App.js
+    <Hello fname="rehan" lname="khan" />
+        This passes fname and lname to hello component as a props
+    {props.fname}
 */
