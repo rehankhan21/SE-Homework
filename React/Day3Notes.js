b795e529467de64d4127765c28a313aaff569832
rehankhan21 <narutop224@gmail.com>
2021-05-05 13:43:54 -0400

    React class components with states

--- React/Day3Notes.js
+++ React/Day3Notes.js
@@ -3,4 +3,15 @@
 
     class component
     -Has its own private data / states
+
+    send the props to the super constructor
+
+    props is an object
+    
+    states is also an object
+
+    props get passed between components, but states cannot pass values between components
+    is this why we use react context api / redux??????
+
+    class needs a render() function to return the value back to root app
 */
