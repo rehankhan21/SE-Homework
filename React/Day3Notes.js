65a74349436ea6b3fefcd366e2850e437e0eb0a6
rehankhan21 <narutop224@gmail.com>
2021-05-05 13:53:00 -0400

    class components with setState

--- React/Day3Notes.js
+++ React/Day3Notes.js
@@ -14,4 +14,17 @@
     is this why we use react context api / redux??????
 
     class needs a render() function to return the value back to root app
+
+    cannot write this.state.count to change counts value but we can write
+    this.state.message to get the value
+
+    too change states value we have to setState() method
+        ex.
+        this.setState({
+             count: this.state.count + 1 //this.state.count++ doesnt work here, not a noraml variable
+        }, () => {
+            console.log(' call back value ' + this.state.count);
+        })
+
+    setState takes 2 arguments
 */
