4cbc85adf3720ce656d0b66ef74ebed27e5513c6
rehankhan21 <narutop224@gmail.com>
2021-05-06 09:29:08 -0400

    array destructing

---
+++ React/Day4Notes.js
@@ -0,0 +1,26 @@
+/*
+    React Day 4
+
+    Using states in functional components,
+
+    Destructing Arrays
+    - extracting multiple properties from an array by taking the strcuture and
+    deconstructing it down into its own parts. this is down by using assignment.
+
+    Destructing Syntax
+    ex. var[first, second, third] = ["denn", "men", "ten"]
+
+    Without Destructing Syntax
+    var first = "denn"
+        second = "men"
+        third = "ten"
+
+    You cannot assign array values to numbers
+
+    This takes each variable on the array literal on the left-hand-side and
+    maps it to the same element at the same index in the array.
+    var thing = ["table", "chair", "fan"]
+
+    var [a, b, c] = thing
+    // a = table, b = chair, c = fan
+*/
