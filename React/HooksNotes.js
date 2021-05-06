a869143952e4aa091a2ce44b2f5ddd228e1fda35
rehankhan21 <narutop224@gmail.com>
2021-05-06 10:22:33 -0400

    React Hooks

--- React/HooksNotes.js
+++ React/HooksNotes.js
@@ -3,5 +3,17 @@
 
     React Hooks
 
-    
+    React States are data that is stored and manipualted and rendered locally, 
+    no connection with the server. This makes it very fast. States can only be used 
+    in React Components.
+
+    you need an initial value when making a state, and a way too change it.
+
+    ** import useStates to use react hooks, this allows you to use states in functional
+    components
+
+    const [count, setCount] = useState(0) // this is array destructing
+
+        useState(0) is our initial value. we always need an initial value for 
+        states.
  */
