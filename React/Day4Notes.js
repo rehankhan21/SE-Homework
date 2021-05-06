b1fe11e011f72cedf7068687a827165a30cc1062
rehankhan21 <narutop224@gmail.com>
2021-05-06 09:44:22 -0400

    The Rest Parameter and Spread Syntax

--- React/Day4Notes.js
+++ React/Day4Notes.js
@@ -43,4 +43,31 @@
     this is like a fallback to avoid undefined error
     var a, b;
     [a = 40, b = -4] = [1, 23]
+
+    Default Values can also refer to other variables including the one
+    in the same array literal
+
+    var [first = coltin, second = first]
+    // seconds output = coltin
+
+    var [first = coltin, second = first] = ["koku"]
+    // seconds ouput = koku
+
+    Igorning Some Values
+    you can skip elements you dont want.
+
+    ex. var a,b 
+    [a, , b] = [lordy, crown, roses]
+    //b = roses;
+
+    ** The Rest Parameter and Spread Syntax
+    The new (...) operator is a REST PARAMETER on the left hand side.
+    A Rest parameter is used to map all the remaining elements in the array
+    that have not been mapped to the rest variable itself. Rest variable must 
+    always be last.
+
+    ex. var planets = ["Mercury", "Earth", "Venus", "Mars", "Pluto", "Saturn"];
+    var [first, , third, ...others] = planet;
+
+    Others output = ["Mars","pluto", "saturn"]
 */
