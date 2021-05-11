bad99fb27fe70e5e9f8519549765f5cc5f2ef6d9
rehankhan21 <narutop224@gmail.com>
2021-05-11 11:01:44 -0400

    day 6 w1hw2 controlled forms

---
+++ React/Day5NotesRendering.js
@@ -0,0 +1,13 @@
+/*
+    Conditional Rendering
+
+    w1hw2 Groceries
+
+    1. use array map for the 3 ojects
+    2. controlled compoenets for inputs
+
+
+    3. a function that will push the new item in array and render it
+    pass the array map
+    conditonal rendering if the item was purchased 
+*/
