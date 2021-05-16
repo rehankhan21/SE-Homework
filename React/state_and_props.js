1b1cc470489cfe674ea6f84c6624eac22c198bfd
rehankhan21 <narutop224@gmail.com>
2021-05-16 18:14:16 -0400

    how to change a parent state from a child component

---
+++ React/state_and_props.js
@@ -0,0 +1,10 @@
+/*
+    How to change a parent component state from a child component?
+    - You can pass a method from the parent component to the child compoent,
+    this method can then take a argument from the child compoenent and pass it back
+    to the parent component then the parent componenet can take this new data as the
+    methods parameter and use setState to change the value of the parent compoenent
+    with whatever value was passed by the child component.
+
+
+ */
