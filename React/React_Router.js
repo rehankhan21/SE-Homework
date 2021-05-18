20c8488f15109535e2520a79847193ce397f4226
rehankhan21 <narutop224@gmail.com>
2021-05-18 10:36:04 -0400

    React Router Link

--- React/React_Router.js
+++ React/React_Router.js
@@ -1,3 +1,27 @@
 /*
+    React Router
+
+    what is it?
+
+    Bring all your components in you root app.
+
+    import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
+
+    BrowseRouter has to have the alias of Router for Router to work, Router wraps the 
+    root apps div tags
+
+    Inside the root apps div tag we have the Switch component which helps us switch
+    between the different Routes(components)
+
+    HTTP request is handled in your form component, with your submit handler
+
+    //exact component is used only for home page
+    <Route path="/" exact component={Home} />
+    <Route path="/" component={About} />
+
+    Link
+
+    import { Link } from "react-router-dom"
+
     
 */
