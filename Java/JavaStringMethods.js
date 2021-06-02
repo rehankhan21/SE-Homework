7965467962165dd419ad64f81121cd49e2cee773
rehankhan21 <narutop224@gmail.com>
2021-06-02 10:47:30 -0400

    java string methods

--- Java/JavaStringMethods.js
+++ Java/JavaStringMethods.js
@@ -13,4 +13,16 @@
     touppercase
     trim
     contains
+
+    charAt in java
+    we have to take the word as a string then scan.next.charAt then
+    store it in one variable.
+
+    **Compare 2 strings in Java
+
+    take 2 inputs, then  
+    name.equals(othername) case sensitive
+    name.equalsIgnoreCase(othername)
+
+    **name.isEmpty(); if the user entered nothing, space bar adds white spaces
 */
