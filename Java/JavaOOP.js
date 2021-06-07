edcce15531bda83f6295a6eb7a3a970de56fe97b
rehankhan21 <narutop224@gmail.com>
2021-06-07 09:36:22 -0400

    Loose Coupling Tight Coupling

---
+++ Java/JavaOOP.js
@@ -0,0 +1,9 @@
+/*
+    Loose Coupling
+    Tight Coupling
+
+    Divide that work in similar categories, This can be in one package. If
+    one part of the website breaks it wont ruin the rest of the website. 
+    Seperate the workflow into different Java Components.
+
+*/
