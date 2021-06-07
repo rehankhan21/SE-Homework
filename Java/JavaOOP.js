722be07fc1ad7adada192351b7d3bc369aeef5f8
rehankhan21 <narutop224@gmail.com>
2021-06-07 10:11:41 -0400

    Loose Coupling Tight Coupling

--- Java/JavaOOP.js
+++ Java/JavaOOP.js
@@ -1,9 +1,14 @@
 /*
-    Loose Coupling
+    Loose Coupling - if one object is not working then u can put that object 
+    undermantaince.
     Tight Coupling
 
     Divide that work in similar categories, This can be in one package. If
     one part of the website breaks it wont ruin the rest of the website. 
     Seperate the workflow into different Java Components.
+
+    Public Level - goes through the whole project, ex store
+    Private Level - only in its own class file.
+    Package Level - only in the package its in, ex.department
 
 */
